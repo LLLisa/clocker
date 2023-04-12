3612f6c890534f0a4382e7c5d3a0a7b2e80f1f82
Lisa Knox <lisaknox83@gmail.com>
2023-04-12 15:53:02 -0400

    initial

---
+++ main.js
@@ -0,0 +1,68 @@
+const { JW_USERNAME, JW_PASSWORD } = require('./secrets');
+
+const puppeteer = require('puppeteer');
+
+const fs = require('fs').promises;
+
+const openBrowser = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  const cookiesString = await fs.readFile('./cookies.json');
+  const oldCookies = JSON.parse(cookiesString);
+  await page.setCookie(...oldCookies);
+
+  // await page.goto('https://secure.justworks.com/login', {
+  await page.goto('https://hours.justworks.com', {
+    waitUntil: 'networkidle2',
+  });
+  const url1 = await page.url();
+  console.log('URL1', url1);
+
+  await page.on.load;
+
+  const loginWJWButton = await page.$('.justworks-log-in-button');
+  if (loginWJWButton) {
+    await loginWJWButton.click();
+  }
+
+  await page.waitForNavigation({
+    waitUntil: 'networkidle0',
+  });
+
+  const url2 = await page.url();
+  console.log('URL2', url2);
+
+  if (url2.includes('secure.justworks.com/login')) {
+    //input username
+    const usernameInput = await page.$('input[name="username"]');
+    await usernameInput.click();
+    await page.keyboard.type(JW_USERNAME);
+
+    //imput password
+    const passwordInput = await page.$('input[name="password"]');
+    await passwordInput.click();
+    await page.keyboard.type(JW_PASSWORD);
+
+    //click submit button
+    const submitButton = await page.$('.Button');
+    await submitButton.click();
+  }
+
+  await page.waitForNavigation({
+    waitUntil: 'networkidle0',
+  });
+
+  const newCookies = await page.cookies();
+  await fs.writeFile('./cookies.json', JSON.stringify(newCookies, null, 2));
+
+  await page.screenshot({ path: 'page.png' });
+
+  const url3 = await page.url();
+  console.log('URL3', url3);
+
+  // await browser.close();
+};
+
+openBrowser();
